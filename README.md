1c5d559dc1684e0847e0ca66e0a9889a8add3242
Luiz Bills <luizbills@pm.me>
2024-03-03 19:41:05 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Timers plugin for litecanvas
+
+Helpers to manage timers in [litecanvas](https://github.com/litecanvas/engine) games.
+
+## Install
+
+**NPM**: `npm i @litecanvas/plugin-timers`
+
+**CDN**: `https://unpkg.com/@litecanvas/plugin-timers/dist/dist.js`
+
+## Usage
+
+```js
+import litecanvas from "@litecanvas/litecanvas"
+import pluginTimers from "@litecanvas/plugin-timers"
+
+litecanvas({
+  plugins: [pluginTimers],
+})
+
+function init() {
+  wait(5, () => {
+    // this function is called after 5 seconds
+  })
+
+  every(0.1, () => {
+    // this function is called every 0.1 seconds (100 ms)
+  })
+
+  repeat(10, 2, () => {
+    // this function is called every 2 seconds (100 ms)
+    // but just 10 times
+  })
+}
+```
---
+++ src/index.js
@@ -0,0 +1,87 @@
+/*! Timers plugin for litecanvas v0.1.0 by Luiz Bills | MIT Licensed */
+window.pluginTimers = plugin
+export default function plugin(engine) {
+  let _timers = []
+
+  // timer class
+  class Timer {
+    constructor(seconds, callback, repeat = false) {
+      this.elapsed = 0
+      this.seconds = seconds
+      this.callback = callback
+      this.repeat = true === repeat ? Infinity : ~~repeat
+      this.id = _timers.push(this)
+    }
+
+    update(dt) {
+      this.elapsed += dt
+      if (this.elapsed >= this.seconds) {
+        this.callback()
+        this.elapsed -= this.seconds
+        if (this.repeat > 1) {
+          this.repeat--
+        } else {
+          this.cancel()
+        }
+      }
+    }
+
+    cancel() {
+      const len = _timers.length
+      if (len > 1 && len !== this.id) {
+        const last = _timers[len - 1]
+        last.id = this.id
+        _timers[last.id - 1] = last
+      }
+      _timers.pop()
+    }
+  }
+
+  // update all timers
+  engine.loop.update.unshift((dt) => {
+    if (0 === _timers.length) return
+    for (const timer of _timers) {
+      timer.update(dt)
+    }
+  })
+
+  return {
+    /**
+     * Wait X seconds and call the callback
+     *
+     * @param {number} seconds time in seconds
+     * @param {function} callback
+     * @return {Timer} this timer instance
+     */
+    wait(seconds, callback) {
+      return new Timer(seconds, callback, false)
+    },
+
+    /**
+     * Call the callback every X seconds
+     *
+     * @param {number} seconds time in seconds
+     * @param {function} callback
+     * @return {Timer} this timer instance
+     */
+    every(seconds, callback) {
+      return new Timer(seconds, callback, true)
+    },
+
+    /**
+     * Repeat the callback N times every X seconds
+     *
+     * @param {number} n times to repeat
+     * @param {number} seconds time in seconds
+     * @param {function} callback
+     * @return {Timer} this timer instance
+     */
+    repeat(n, seconds, callback) {
+      return new Timer(seconds, callback, n)
+    },
+  }
+}
+
+pluginTimers.priority = 0 // run this plugin before other plugins
+
+window.pluginTimers = pluginTimers
